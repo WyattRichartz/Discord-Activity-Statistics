3a6556d44b1ff78cfe449892b647966016c3946c
ChobaniMan <54227960+WyattRichartz@users.noreply.github.com>
2023-07-06 01:12:38 -0700

    Temp Update 1
    Begun using OOP, code already looks much nicer

--- Discord Server Stats.py
+++ Discord Server Stats.py
@@ -1,13 +1,12 @@
 from datetime import timedelta, timezone, datetime
 from discord.ext import commands
-# import openpyxl
 import discord
+# import openpyxl
 import pandas as pd
 import numpy as np
 import time
 
 #Need version 1.7.3 of discord.py
-
 
 #!Make it so that you can only measure channels in specific categories using Guild.category
 #!Use multiprocessing/threading (multiprocessessing might be faster) to speed up indexing times
@@ -28,207 +27,188 @@ import time
 #*Add options to see the following properties of individual users (might want to use user class instead of member class when applicable because it is faster):
 #*public_flags, created_at, bot, nick, messages per day, and add option to render their avatar in the spreadsheet
 
-id_of_server = 
-id_of_channels = []
+ID_OF_SERVER = 738192523352211456
+channels_to_measure = [] #leave empty to measure all channels
 message_capture_limit = None #Input None to measure the entire channel
-ids_to_measure_dates = []
-MeasureAllChannels = False
-ReplaceZeroesWithEmptyCells = False
-MeasureByPeople = True
-MeasureAllDatesPeople = True
-MeasureByDates = False
+replace_zeroes_with_empty_cells = False
+measure_by_people = True
+measure_dates_per_person = True
+measure_by_dates = False
 file_save_location = r'D:\Programs\Python\Discord Spreadsheets'
-token = ''
-
-start_time = time.time()
-df_list = []
-guild_list = []
-df_list_by_dates = []
-day_counter_list_for_days = []
-new_df_list = []
-total_messages_list = []
-total_messages_list_by_dates = []
-see_if_dates_checker = []
-list_of_columns = []
-aggregation_functions = {}
-aggregation_functions_by_dates = {}
-total_messages = 0
-total_messages_by_dates = 0
-counter = 0
+token = 'NzEyMTQ3NjM4NDU5MzAxOTcw.Ge-shg.zswJBozQNJhvK5JGVZi8g8E9SB0MzzK2tmEW0w'
 
 now = datetime.now()
+now_utc = datetime.utcnow()
 current_time = now.strftime("%H:%M:%S")
 print(current_time)
 
-for key, element in enumerate(ids_to_measure_dates):
-    ids_to_measure_dates[key] = str(element)
+class Author:
+    def __init__(self, message, DATE_DICT_ZEROES):
+        self.author = message.author
+        self.author_id = self.author.id
+        self.author_name = self.get_author_name()
+        # self.author_avatar = self.author.display_icon
+        self.message_total = 0
+        self.in_server = self.in_server_check()
+        if DATE_DICT_ZEROES is not None:
+            self.message_dates_dict = DATE_DICT_ZEROES.copy()
+            self.message_dates_sent = []
+        del self.author
 
-def daterange(date1, date2):
-    for n in range(int((date2 - date1).days)+1):
-        yield date1 + timedelta(n)
+    def in_server_check(self):
+        if type(self.author) == discord.Member:
+            return True
+        elif type(self.author) == discord.User:
+            return False
+    
+    def get_author_name(self):
+        if self.author.discriminator != '0':
+            return f'{self.author.name}#{self.author.discriminator}'
+        else:
+            return self.author.name
+    
+    def update_message_totals(self, message):
+        if self.message_dates_dict is not None:
+            self.message_dates_dict[message.created_at.date()] += 1
+            self.message_dates_sent.append(message.created_at.date())
+        self.message_total += 1
+        print(self.author_name)
+    
+    def get_min_max_dates(self):
+        self.earliest_date = min(self.message_dates_sent)
+        self.latest_date = max(self.message_dates_sent)
+        self.days_since_first_message = (now_utc.date() - self.earliest_date).days
+        self.days_since_last_message = (now_utc.date() - self.latest_date).days
+        del self.message_dates_sent
+    
+    def make_values_nan(self):
+        for date in self.message_dates_dict:
+            if self.message_dates_dict[date] == 0:
+                self.message_dates_dict[date] = np.nan
+    
+    def output_dataframe_values(self):
+        output = {}
+        temp_variables = vars(self)
+        output.update(temp_variables['message_dates_dict'])
+        del temp_variables['message_dates_dict']
+        output.update(temp_variables)
+        return output
 
-def to_integer(dt_time):
-    return 10000*dt_time.year + 100*dt_time.month + dt_time.day
+start_time = time.time()
+df_list = []
+df_list_by_dates = []
+day_counter_list_for_days = []
+new_df_list = []
+list_of_columns = []
+aggregation_functions = {}
+aggregation_functions_by_dates = {}
+
+
+def common_list_members(list1, list2):
+    '''
+    Outputs a list of all of the elements that are in both list1 and list2 (outputs the intersection of the two sets).
+    '''
+    set1, set2 = set(list1), set(list2)
+    return set1.intersection(set2)
+
+def channel_list_creator(guild, self_user, bounding_channel_list):
+    '''
+    Takes as input a guild, your user, and the channels to include and outputs a list of all of the channels that your account has access to.
+    This is to ensure that you have permissions to view the message history of all of the channels that are being analyzed.
+    If bounding_channel_list is not an empty list, then the output will not contain any channels that are not in the bounding channel list.
+    '''
+    id_of_channels = []
+    for channel in guild.text_channels:
+        if channel.permissions_for(self_user).read_message_history == True:
+            id_of_channels.append(channel.id)
+    
+    if len(bounding_channel_list) != 0:
+        id_of_channels = common_list_members(id_of_channels, bounding_channel_list)
+    return id_of_channels
 
 client = commands.Bot(command_prefix='>', self_bot=True)
 @client.event
 async def on_ready():
     global id_of_channels
-    guild = client.get_guild(id_of_server)
-    permissions_list = []
-    self_user = guild.me
+    DATE_DICT_ZEROES, date_dict_values = {}, {}
+    guild = client.get_guild(ID_OF_SERVER)
+    id_of_channels = channel_list_creator(guild, guild.me, channels_to_measure)
     
-    if MeasureAllChannels:
-        id_of_channels = []
-        for channel in guild.text_channels:
-            for value in self_user.permissions_in(channel):
-                permissions_list.append(value)
-            if permissions_list[10][-1] == True:
-                id_of_channels.append(channel.id)
-                permissions_list = []
-
-    for id_of_channel in id_of_channels:
-        date_list = []
-        empty_list = []
-        list_counter_list = []
-        zeros_list = []
-        unique_author_id_list = []
-        author_dict = {}
-        counter = 0
-        total_messages = 0
-        counter_dates = 1
-        day_counter_list = []
-        date_dict = {}
-        channel = client.get_channel(id_of_channel)
-        creation_date = datetime.strptime(str(guild.created_at).split(' ')[0], '%Y-%m-%d').date()
-        
-        for dt in daterange(creation_date, datetime.now(timezone.utc).date()):
-            date_list.append(str(dt.strftime("%Y-%m-%d")))
-            list_of_columns.append(date_list[-1])
-            zeros_list.append(0)
-            empty_list.append(np.nan)
-            
+    if measure_dates_per_person:
+        date_list = pd.date_range(guild.created_at.date(), datetime.now(timezone.utc).date(), freq='D')
+        date_list = [pd.to_datetime(i).date() for i in date_list]
         for date in date_list:
-            date_dict[date] = [counter_dates, date, 0]
-            day_counter_list.append(counter_dates)
-            counter_dates += 1
-            
+            DATE_DICT_ZEROES[date] = 0
+            date_dict_values[date] = 0
+    else:
+        DATE_DICT_ZEROES = None
+    
+    for channel_id in id_of_channels:
+        author_dict = {}
+        channel = client.get_channel(channel_id)
+        
+        total_messages = 0
         async for message in channel.history(limit=message_capture_limit):
             total_messages += 1
-            member = message.author
-            author_id = member.id
-            author_name = member.name
-            author_tag = '#' + member.discriminator
-            message_date = str(message.created_at).split(' ')[0]
-            message_date_int =  to_integer(datetime.strptime(message_date, "%Y-%m-%d"))
+            author = message.author
             
-            if MeasureByPeople:
-                if author_id in unique_author_id_list:
-                    author_dict[author_id][4] = (datetime.now(timezone.utc).date() - datetime.strptime(message_date, "%Y-%m-%d").date()).days
-                    author_dict[author_id][3] = to_integer(datetime.strptime(message_date, "%Y-%m-%d"))
-                    author_dict[author_id][2] += 1
-                    if not MeasureAllDatesPeople:
-                        if author_id in ids_to_measure_dates:
-                            date_counter = date_list.index(message_date)
-                            author_dict[author_id][9 + date_counter] += 1 #added columns
-                            list_counter_list.append(0)
-                            see_if_dates_checker.append(0)
-                    else:
-                        date_counter = date_list.index(message_date)
-                        author_dict[author_id][9 + date_counter] += 1 #added columns
-                        list_counter_list.append(0)
-                        see_if_dates_checker.append(0)
-                        
-                else:
-                    try:
-                        await guild.fetch_member(author_id)
-                        user_in_server = 'Yes'
-                    except:
-                        user_in_server = 'No'
-                    author_dict[author_id] = [author_tag, author_name, 1, message_date_int, (datetime.now(timezone.utc).date() - datetime.strptime(message_date, "%Y-%m-%d").date()).days, message_date_int, (datetime.now(timezone.utc).date() - datetime.strptime(message_date, "%Y-%m-%d").date()).days, user_in_server, int(author_id)]
-                    unique_author_id_list.append(author_id)
-                    if not MeasureAllDatesPeople:
-                        if author_id in ids_to_measure_dates:
-                            author_dict[author_id].extend(zeros_list)
-                            date_counter = date_list.index(message_date)
-                            author_dict[author_id][9 + date_counter] += 1 #added columns
-                            list_counter_list.append(0)
-                            see_if_dates_checker.append(0)
-                        else:
-                            author_dict[author_id].extend(empty_list)
-                    else:
-                        author_dict[author_id].extend(zeros_list)
-                        date_counter = date_list.index(message_date)
-                        author_dict[author_id][9 + date_counter] += 1 #added columns
-                        list_counter_list.append(0)
-                        see_if_dates_checker.append(0)
+            if measure_by_people:
+                if not author.id in author_dict:
+                    author_dict[author.id] = Author(message, DATE_DICT_ZEROES)
+                author_dict[author.id].update_message_totals(message)
 
-            if MeasureByDates:
-                date_dict[message_date][-1] += 1
+            if measure_by_dates:
+                date_dict_values[message.created_at] += 1
                 
             if total_messages % 10000 == 0:
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
                 print(f'{current_time} - {total_messages // 1000}k - {channel}')
-                
-        if MeasureByPeople:
-            for id in unique_author_id_list:
-                if len(list_counter_list) == 0:
-                    author_dict[id] = author_dict[id][0:5]
-                author_dict[counter] = author_dict.pop(id)
-                if author_dict[counter][0] == '#0000':
-                    del author_dict[counter]
-                counter += 1
-            
-            columns_list = ['Tag', 'Name', f'({total_messages}) Message Total', 'First Message Date', 'Days Since First Message', 'Last Message Date', 'Days Since Last Message', 'Still in Server?', 'User ID']
-            if len(list_counter_list) != 0:
-                columns_list.extend(date_list)
-            df = pd.DataFrame(author_dict.values(), index=author_dict.keys(), columns=columns_list)
-            df['User ID'] = df['User ID'].apply('="{}"'.format)
-            df['Name'] = df['Name'].apply('="{}"'.format)
-            if ReplaceZeroesWithEmptyCells:
-                counter3 = 0
-                for column in df:
-                    if counter3 < 8:
-                        pass
-                    else:
-                        df[column] = df[column].replace(0, np.nan)
-                    counter3 += 1
-            df_export = df.copy()
-            df_export['First Message Date'] = df_export['First Message Date'].apply(lambda element: f'{str(element)[0:4]}-{str(element)[4:6]}-{str(element)[6:]}')
-            df_export['Last Message Date'] = df_export['Last Message Date'].apply(lambda element: f'{str(element)[0:4]}-{str(element)[4:6]}-{str(element)[6:]}')
-            df_export.to_csv(f'{file_save_location}\\{channel} by Users, {guild}.csv', index=False, encoding='utf-8-sig')   
-            df_list.append(df)
-            total_messages_list.append(total_messages)
-            print(df_export)
         
-        if MeasureByDates:
-            columns_list_by_dates = [f'({counter_dates}) Day Counter', 'Date', f'({total_messages}) Message Total']
-            df = pd.DataFrame(date_dict.values(), index=day_counter_list, columns=columns_list_by_dates)
+        if measure_dates_per_person:
+            for author in author_dict:
+                author_dict[author].get_min_max_dates()
+            columns_list = ['author_name', f'message_total', 'earliest_date', 'days_since_first_message', 'latest_date', 'days_since_last_message', 'in_server', 'author_id']
+            for date in date_list:
+                columns_list.append(date)
+            print(columns_list)
+            for i in author_dict.keys():
+                author_dict[i] = author_dict[i].output_dataframe_values()
+            df = pd.DataFrame(data=author_dict.values(), columns=columns_list)
+            df['author_id'] = df['author_id'].apply('="{}"'.format)
+            df['author_name'] = df['author_name'].apply('="{}"'.format)
+            df.to_csv(f'{file_save_location}\\{channel} by Users, {guild}.csv', index=False, encoding='utf-8-sig')   
+            df_list.append(df)
+            print(df)
+            time.sleep(20)
+        
+        if measure_by_dates:
+            columns_list_by_dates = [f'({len(date_list)}) Day Counter', 'Date', f'({total_messages}) Message Total']
+            df = pd.DataFrame(date_dict_values.values(), columns=columns_list_by_dates)
             df.to_csv(f'{file_save_location}\\{channel} by Dates, {guild}.csv', index=False, encoding='utf-8-sig')
             df_list_by_dates.append(df)
             total_messages_list_by_dates.append(total_messages)
             day_counter_list_for_days.append(counter)
             print(df)
+        
     await client.close()
 client.run(token)
 
-if MeasureByPeople:
+if measure_by_people:
     for number in total_messages_list:
         total_messages += number
     final_message_count = total_messages
 
-if MeasureByDates:
+if measure_by_dates:
     for number in total_messages_list_by_dates:
         total_messages_by_dates += number
     final_message_count = total_messages_by_dates
 
-if MeasureByPeople:
+if measure_by_people:
     for df in df_list:
         df = df.rename(columns={df.columns[2]: 'Message Total'})
         new_df_list.append(df)
     new_df_temp = pd.concat(new_df_list)
-    aggregation_functions['Tag'] = 'first'
     aggregation_functions['Name'] = 'first'
     aggregation_functions['Message Total'] = 'sum'
     aggregation_functions['First Message Date'] = 'min'
@@ -241,13 +221,13 @@ if MeasureByPeople:
     new_df_temp['First Message Date'] = new_df_temp['First Message Date'].apply(lambda element: f'{str(element)[0:4]}-{str(element)[4:6]}-{str(element)[6:]}')
     new_df_temp['Last Message Date'] = new_df_temp['Last Message Date'].apply(lambda element: f'{str(element)[0:4]}-{str(element)[4:6]}-{str(element)[6:]}')
         
-    if len(see_if_dates_checker) != 0:
-        for date in list_of_columns:
+    if len(date_list) != 0:
+        for date in date_list:
             aggregation_functions[date] = 'sum'
 
     new_df = new_df_temp.groupby(new_df_temp['User ID']).aggregate(aggregation_functions)
     new_df = new_df.rename(columns={'Message Total': f'({total_messages}) Message Total'})
-    if ReplaceZeroesWithEmptyCells:
+    if replace_zeroes_with_empty_cells:
         counter2 = 0
         for column in new_df:
             if counter2 < 8:
@@ -255,10 +235,10 @@ if MeasureByPeople:
             else:
                 new_df[column] = new_df[column].replace(0, np.nan)
             counter2 += 1
-    new_df.to_csv(f'{file_save_location}\\Combined Channels by Users, {client.get_guild(id_of_server)}.csv', index=False, encoding='utf-8-sig')
+    new_df.to_csv(f'{file_save_location}\\Combined Channels by Users, {client.get_guild(ID_OF_SERVER)}.csv', index=False, encoding='utf-8-sig')
     print(new_df)
 
-if MeasureByDates:
+if measure_by_dates:
     for df in df_list_by_dates:
         df = df.rename(columns={df.columns[0]: 'Day Counter'})
         df = df.rename(columns={df.columns[2]: 'Message Total'})
@@ -271,7 +251,7 @@ if MeasureByDates:
     new_df_by_dates = new_df_temp_by_dates.groupby(new_df_temp_by_dates['Date']).aggregate(aggregation_functions_by_dates)
     new_df_by_dates = new_df_by_dates.rename(columns={'Message Total': f'({total_messages_by_dates}) Message Total'})
     new_df_by_dates = new_df_by_dates.rename(columns={'Day Counter': f'({day_counter_list_for_days[-1]}) Day Counter'})
-    new_df_by_dates.to_csv(f'{file_save_location}\\Combined Channels by Dates, {client.get_guild(id_of_server)}.csv', index=False, encoding='utf-8-sig')
+    new_df_by_dates.to_csv(f'{file_save_location}\\Combined Channels by Dates, {client.get_guild(ID_OF_SERVER)}.csv', index=False, encoding='utf-8-sig')
     print(new_df_by_dates)
 
 end_time = int(time.time() - start_time)
